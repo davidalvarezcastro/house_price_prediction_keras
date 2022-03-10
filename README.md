73e32d450ea8474d56b03ed8fb13e485c4c806d5
David Álvarez Castro <davidac0291@gmail.com>
2022-03-10 20:15:22 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# House Price Predictions using Keras
+
+Proyecto de ejemplo utilizando Keras para crear un modelo (regresión lineal) que permita predecir el precio de una casa a partir de una foto.
---
+++ main.py
@@ -0,0 +1,74 @@
+"""
+    Example taken from https://towardsai.net/p/deep-learning/house-price-predictions-using-keras
+    Author: towardsai
+"""
+
+# import required libraries
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn import metrics
+# Creating a Neural Network Model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.optimizers import Adam
+
+
+Data = pd.read_csv('./input/kc_house_data.csv')
+
+# let's drop unnecessory featurs
+Data = Data.drop('id',axis=1)
+Data = Data.drop('zipcode',axis=1)
+
+Data = Data.drop('date',axis=1)
+
+X = Data.drop('price',axis =1).values
+y = Data['price'].values
+
+#splitting Train and Test 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=101)
+
+#standardization scaler - fit&transform on train, fit only on test
+s_scaler = StandardScaler()
+X_train = s_scaler.fit_transform(X_train.astype(np.float64))
+X_test = s_scaler.transform(X_test.astype(np.float64))
+
+print(X_train.shape)
+
+# having 19 nueron is based on the number of available features
+
+model = Sequential()
+
+model.add(Dense(17,activation='relu'))
+model.add(Dense(17,activation='relu'))
+model.add(Dense(17,activation='relu'))
+model.add(Dense(17,activation='relu'))
+model.add(Dense(1))
+
+model.compile(optimizer='adam',loss='mse')
+
+# training
+model.fit(x=X_train,y=y_train,
+          validation_data=(X_test,y_test),
+          batch_size=128,epochs=400)
+model.summary()
+
+# evaluation
+y_pred = model.predict(X_test)
+
+# evaluation metrics
+# explained variance score: best possible score is 1 and lower values are worse
+print('Mean Absolute Error: {:.2f}'.format(metrics.mean_absolute_error(y_test, y_pred)))
+print('Mean Squared Error: {:.2f}'.format(metrics.mean_squared_error(y_test, y_pred)))
+print('Root Mean Squared Error: {:.2f}'.format(np.sqrt(metrics.mean_squared_error(y_test, y_pred))))
+print('Variance score is: {:.2f}'.format(metrics.explained_variance_score(y_test,y_pred)))
+
+# example
+single_house = Data.drop('price',axis = 1).iloc[0]
+single_house = s_scaler.transform(single_house.values.reshape(-1,17))
+prediction = model.predict(single_house)
+
+print(f"Real value: {Data['price'][0]} ==> {prediction[0][0]}")
